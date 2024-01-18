84a435997dfb33a516f81e73a8d13fcbb09c2710
Anaïs <aekyon.08@gmail.com>
2024-01-18 11:35:47 +0100

    Connect database and handle errors

--- Controller/ArticleController.php
+++ Controller/ArticleController.php
@@ -4,6 +4,12 @@ declare(strict_types = 1);
 
 class ArticleController
 {
+    private DatabaseManager $databaseManager;
+
+    public function __construct(DatabaseManager $databaseManager) {
+        $this->databaseManager = $databaseManager;
+    }
+
     public function index()
     {
         // Load all required data
@@ -18,17 +24,28 @@ class ArticleController
     {
         // TODO: prepare the database connection
         // Note: you might want to use a re-usable databaseManager class - the choice is yours
+
         // TODO: fetch all articles as $rawArticles (as a simple array)
-        $rawArticles = [];
+        try {
+            $query = "SELECT * FROM articles;";
 
-        $articles = [];
-        foreach ($rawArticles as $rawArticle) {
-            // We are converting an article from a "dumb" array to a much more flexible class
-            $articles[] = new Article($rawArticle['title'], $rawArticle['description'], $rawArticle['publish_date']);
+            $statement = $this->databaseManager->connection->prepare($query);
+            $statement->execute();
+            $rawArticles = $statement->fetchAll();
+
+            $articles = [];
+            foreach ($rawArticles as $rawArticle) {
+                // We are converting an article from a "dumb" array to a much more flexible class
+                $articles[] = new Article($rawArticle['title'], $rawArticle['description'], $rawArticle['publish_date']);
+            }
+
+            return $articles;
+
+        } catch (PDOException $e) {
+            echo("Select query failed" . $e->getMessage());
         }
-
-        return $articles;
     }
+
 
     public function show()
     {

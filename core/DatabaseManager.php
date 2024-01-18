14c89f0d93b398e519284fa77ff04b6fb318cd6d
Anaïs <aekyon.08@gmail.com>
2024-01-18 11:06:15 +0100

    Provide database information to link

---
+++ core/DatabaseManager.php
@@ -0,0 +1,39 @@
+<?php
+require_once 'config.php';
+// This class will manage the connection to the database
+// It will be passed on the other classes who need it
+class DatabaseManager
+{
+    // These are private: only this class needs them
+    private string $host;
+    private string $user;
+    private string $password;
+    private string $dbname;
+    // This one is public, so we can use it outside of this class
+    // We could also use a private variable and a getter (but let's not make things too complicated at this point)
+    public PDO $connection;
+
+    public function __construct(string $host, string $user, string $password, string $dbname)
+    {
+        $this->host = $host;
+        $this->user = $user;
+        $this->password = $password;
+        $this->dbname = $dbname;
+    }
+
+    public function connect(): void
+    {
+        try {
+            //if custom port, add to dsn
+            $dsn = "mysql:host=$this->host;dbname=$this->dbname";
+            $this->connection = new PDO($dsn, $this->user, $this->password);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+            echo 'DatabaseManager.php is connected to the database';
+
+        } catch (PDOException $error) {
+            echo 'DatabaseManager.php failed to connect: ' . $error->getMessage();
+        }
+   }
+}

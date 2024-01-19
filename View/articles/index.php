5ddc3f997367d1585d7ada4053b35f21287f695e
Anaïs <aekyon.08@gmail.com>
2024-01-19 12:31:33 +0100

    Revert to original for debugging

--- View/articles/index.php
+++ View/articles/index.php
@@ -1,17 +1,23 @@
 <?php require 'View/includes/header.php'?>
 
-<?php // Use any data loaded in the controller here ?>
+<?php // Use any data loaded in the controller here 
+//<p>Summary: <?= $article->getDescription();</p> ;
+//<p><a href="index.php?page=articles-show&id=<?= $article->getId();
+//>Read More</a></p>?>
 
 <section>
     <h1>Articles</h1>
     <ul>
         <?php foreach ($articles as $article) : ?>
             <h2><?= $article->getTitle(); ?></h2>
-            <p>Summary: <?= $article->getDescription(); ?></p>
             <span>Date published: <?= $article->formatPublishDate(); ?></span>
-            <a href="index.php?page=articles-show&id=<?= $article->getId(); ?>">Read More</a>
+            <p><a href="index.php?page=articles-show&id=<?= $article->getId(); ?>">Read More</a></p>
+
+
+            
         <?php endforeach; ?>
     </ul>
 </section>
 
 <?php require 'View/includes/footer.php'?>
+
